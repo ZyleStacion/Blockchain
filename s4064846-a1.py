7cc928215eab08bda5dda1285d99a9fced52275a
ZyleStacion <s4064846@rmit.edu.vn>
2025-07-10 13:52:40 +0700

    use digest for XOR

--- s4064846-a1.py
+++ s4064846-a1.py
@@ -11,14 +11,14 @@ def question1():
     # Update the hash object with our user input
     hash.update(user_input.encode('utf-8'))
 
-    # Display the hash in hex format
-    print(hash.hexdigest())
-
     # Step 3: Demonstrate the avalanche effect by flipping a single bit
-    # Convert user_input to binary (https://www.geeksforgeeks.org/python/python-convert-string-to-binary/)
+    
+    # Convert user_input to binary 
+    # Source: https://www.geeksforgeeks.org/python/python-convert-string-to-binary/
     binary_input = ''.join(format(ord(char), '08b') for char in user_input)
 
-    print("Flipped string: ", binary_input)
+    print("\nFlipped string: ", binary_input)
+    
     # Flip the first bit
     first_bit = binary_input[0]
 
@@ -29,15 +29,42 @@ def question1():
     
     print("Unflipped string: ", binary_input)
 
+    # Convert the binary string into an encoded, hashable string
+    # Source: https://stackoverflow.com/questions/32675679/convert-binary-string-to-bytearray-in-python-3
+    modified_input = int(binary_input, 2).to_bytes((len(binary_input) + 7) // 8)
+
+    # Create the new hash
     modified_hash = hashlib.sha256()
-
-    # Update the hash object with our user input
-    # modified_hash.update(modified_input.encode('utf-8'))
-
-    # print(modified_hash.hexdigest())
-
-    # Highlight the difference between the two hashes
+    modified_hash.update(modified_input)
     
+    # Highlight the difference between the two hashes
+    hash_digest = hash.hexdigest()
+    modified_digest = modified_hash.hexdigest()
+    
+    print("\nOld Hash: " + hash_digest)
+    print("New Hash: " + modified_digest)
+
+    # Convert both hashes into binary format
+    LENGTH = 256
+
+    # Use .digest() method here for accuracy
+    binary_hash = ''.join(format(ord(char), '08b') for char in hash.digest())
+    binary_modified = ''.join(format(ord(char), '08b') for char in modified_hash.digest())
+
+    # XOR the hashes to look for differences
+    xor_string = ''
+    flipped_bits = 0
+
+    for i in range(LENGTH):
+        current = str(int(binary_hash[i]) ^ int(binary_modified[i]))
+        xor_string += current
+
+        if current == '1':
+            flipped_bits += 1
+    
+    print(flipped_bits)
+    avalance_effect = flipped_bits / LENGTH
+    print("Avalance Effect = ", avalance_effect)
 
     # Return hash value for use in pre-image testing
     return hash
